a9f7b3a40d655f5b7ec5d2c851c6b4713588a643
Praneeth Kodavati <kssmpranith@gmail.com>
2025-10-23 14:42:24 +0530

    Added support for sparse mul

--- src/assembler/parser.cpp
+++ src/assembler/parser.cpp
@@ -304,6 +304,11 @@ void Parser::parseTextDirective() {
             break;
           }
 
+          case instruction_set::SyntaxType::O_GPR_C_GPR_C_GPR_C_GPR: {
+            valid_syntax = parse_O_GPR_C_GPR_C_GPR_C_GPR();
+            break;
+          }
+
           case instruction_set::SyntaxType::O_GPR_C_GPR_C_I: {
             valid_syntax = parse_O_GPR_C_GPR_C_I();
             break;
